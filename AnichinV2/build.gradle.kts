e24a8d7e54ebc9b3397aa9e2c9943ec1195fb55c
kurato-svg <kuratotv@gmail.com>
2026-08-05 13:04:10 +0800

    Create build.gradle.kts

---
+++ AnichinV2/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "Anichin V2 — Streaming Donghua Subtitle Indonesia"
+    language = "id"
+    authors = listOf("Kurato")
+    status = 3
+    tvTypes = listOf("AnimeMovie", "Anime", "Cartoon")
+}
