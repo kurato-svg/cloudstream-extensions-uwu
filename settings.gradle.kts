22c722f4b0ddea4214fa75c017894f59004eda20
kurato-svg <kuratotv@gmail.com>
2026-08-05 18:39:17 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "CloudstreamPlugins"
 
 // Temporary: build only AnichinV2Provider
 include("AnichinV2Provider")
+include("OppaDramaProvider")
 
 // Auto-discovery disabled temporarily
 // val disabled = listOf<String>()
