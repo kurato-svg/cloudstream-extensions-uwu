5d6a2e4bd684bdafc5457b503e395c70e46b69e6
kurato-svg <kuratotv@gmail.com>
2026-08-05 13:01:28 +0800

    Update build.gradle.kts

--- AnichinProvider/src/main/kotlin/com/anichin/AnichinV2/build.gradle.kts
+++ AnichinProvider/src/main/kotlin/com/anichin/AnichinV2/build.gradle.kts
@@ -1,1 +1,9 @@
-description = "Anichin V2 CloudStream Provider"
+version = 1
+
+cloudstream {
+    description = "Anichin V2 — Streaming Donghua Subtitle Indonesia"
+    language = "id"
+    authors = listOf("Kurato")
+    status = 3
+    tvTypes = listOf("AnimeMovie", "Anime", "Cartoon")
+}
