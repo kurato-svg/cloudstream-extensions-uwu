d8425804f0a8f8a488417ea8cdfbee2fe52fef83
kurato-svg <kuratotv@gmail.com>
2026-08-05 13:13:42 +0800

    Delete AnichinV2 directory

--- AnichinV2/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "Anichin V2 — Streaming Donghua Subtitle Indonesia"
-    language = "id"
-    authors = listOf("Kurato")
-    status = 3
-    tvTypes = listOf("AnimeMovie", "Anime", "Cartoon")
-}
