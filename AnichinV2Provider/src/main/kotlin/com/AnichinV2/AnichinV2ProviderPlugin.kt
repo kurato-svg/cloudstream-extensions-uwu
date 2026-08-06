11a0cdbe05b04b2b76683b999a227a3c4d0535e9
kurato-svg <kuratotv@gmail.com>
2026-08-06 17:52:41 +0800

    Update AnichinV2ProviderPlugin.kt

--- AnichinV2Provider/src/main/kotlin/com/AnichinV2/AnichinV2ProviderPlugin.kt
+++ AnichinV2Provider/src/main/kotlin/com/AnichinV2/AnichinV2ProviderPlugin.kt
@@ -8,19 +8,60 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class AnichinV2ProviderPlugin : Plugin() {
 
     override fun load(context: Context) {
-        registerMainAPI(AnichinV2())
-        registerExtractorAPI(Dailymotion())
-        registerExtractorAPI(Geodailymotion())
-        registerExtractorAPI(Odnoklassniki())
-        registerExtractorAPI(OkRuSSL())
-        registerExtractorAPI(OkRuHTTP())
-        registerExtractorAPI(Rumble())
-        registerExtractorAPI(StreamRuby())
-        registerExtractorAPI(svilla())
-        registerExtractorAPI(svanila())
-        registerExtractorAPI(Vidguardto())
-        registerExtractorAPI(Vidguardto1())
-        registerExtractorAPI(Vidguardto2())
-        registerExtractorAPI(Vidguardto3())
+        registerMainAPI(
+            AnichinV2()
+        )
+
+        registerExtractorAPI(
+            Dailymotion()
+        )
+
+        registerExtractorAPI(
+            Geodailymotion()
+        )
+
+        registerExtractorAPI(
+            Odnoklassniki()
+        )
+
+        registerExtractorAPI(
+            OkRuSSL()
+        )
+
+        registerExtractorAPI(
+            OkRuHTTP()
+        )
+
+        registerExtractorAPI(
+            Rumble()
+        )
+
+        registerExtractorAPI(
+            StreamRuby()
+        )
+
+        registerExtractorAPI(
+            svilla()
+        )
+
+        registerExtractorAPI(
+            svanila()
+        )
+
+        registerExtractorAPI(
+            Vidguardto()
+        )
+
+        registerExtractorAPI(
+            Vidguardto1()
+        )
+
+        registerExtractorAPI(
+            Vidguardto2()
+        )
+
+        registerExtractorAPI(
+            Vidguardto3()
+        )
     }
 }
