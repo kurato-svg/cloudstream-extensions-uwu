81015483bfe828923f84c00f8bd9bc0cf0e2d77b
kurato-svg <kuratotv@gmail.com>
2026-08-06 18:28:18 +0800

    Update AnichinV2ProviderPlugin.kt

--- AnichinV2Provider/src/main/kotlin/com/AnichinV2/AnichinV2ProviderPlugin.kt
+++ AnichinV2Provider/src/main/kotlin/com/AnichinV2/AnichinV2ProviderPlugin.kt
@@ -5,63 +5,22 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AnichinV2ProviderPlugin : Plugin() {
-
+class AnichinV2ProviderPlugin: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(
-            AnichinV2()
-        )
+        registerMainAPI(AnichinProvider())
+        registerExtractorAPI(Dailymotion())
+        registerExtractorAPI(Geodailymotion())
+        registerExtractorAPI(Odnoklassniki())
+        registerExtractorAPI(OkRuSSL())
+        registerExtractorAPI(OkRuHTTP())
+        registerExtractorAPI(Rumble())
+        registerExtractorAPI(StreamRuby())
+        registerExtractorAPI(svilla())
+        registerExtractorAPI(svanila())
+        registerExtractorAPI(Vidguardto())
+        registerExtractorAPI(Vidguardto1())
+        registerExtractorAPI(Vidguardto2())
+        registerExtractorAPI(Vidguardto3())
 
-        registerExtractorAPI(
-            Dailymotion()
-        )
-
-        registerExtractorAPI(
-            Geodailymotion()
-        )
-
-        registerExtractorAPI(
-            Odnoklassniki()
-        )
-
-        registerExtractorAPI(
-            OkRuSSL()
-        )
-
-        registerExtractorAPI(
-            OkRuHTTP()
-        )
-
-        registerExtractorAPI(
-            Rumble()
-        )
-
-        registerExtractorAPI(
-            StreamRuby()
-        )
-
-        registerExtractorAPI(
-            svilla()
-        )
-
-        registerExtractorAPI(
-            svanila()
-        )
-
-        registerExtractorAPI(
-            Vidguardto()
-        )
-
-        registerExtractorAPI(
-            Vidguardto1()
-        )
-
-        registerExtractorAPI(
-            Vidguardto2()
-        )
-
-        registerExtractorAPI(
-            Vidguardto3()
-        )
     }
 }
