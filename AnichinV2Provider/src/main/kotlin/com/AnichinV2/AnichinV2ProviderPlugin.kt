85d988c168045d2587fe6818f6700aa832003e2e
kurato-svg <kuratotv@gmail.com>
2026-08-06 19:42:41 +0800

    Update AnichinV2ProviderPlugin.kt

--- AnichinV2Provider/src/main/kotlin/com/AnichinV2/AnichinV2ProviderPlugin.kt
+++ AnichinV2Provider/src/main/kotlin/com/AnichinV2/AnichinV2ProviderPlugin.kt
@@ -9,5 +9,18 @@ class AnichinV2ProviderPlugin : Plugin() {
 
     override fun load(context: Context) {
         registerMainAPI(AnichinV2())
+        registerExtractorAPI(Dailymotion())
+        registerExtractorAPI(Geodailymotion())
+        registerExtractorAPI(Odnoklassniki())
+        registerExtractorAPI(OkRuSSL())
+        registerExtractorAPI(OkRuHTTP())
+        registerExtractorAPI(Rumble())
+        registerExtractorAPI(StreamRuby())
+        registerExtractorAPI(svilla())
+        registerExtractorAPI(svanila())
+        registerExtractorAPI(Vidguardto())
+        registerExtractorAPI(Vidguardto1())
+        registerExtractorAPI(Vidguardto2())
+        registerExtractorAPI(Vidguardto3())
     }
 }
