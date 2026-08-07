ab093763ccbf5caa16ccfa7d08b561312e57da1d
kurato-svg <kuratotv@gmail.com>
2026-08-07 15:49:09 +0800

    Update OppadramaProviderPlugin.kt

--- OppaDramaProvider/src/main/kotlin/com/oppadrama/OppadramaProviderPlugin.kt
+++ OppaDramaProvider/src/main/kotlin/com/oppadrama/OppadramaProviderPlugin.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
-
 @CloudstreamPlugin
 class OppadramaProviderPlugin : Plugin() {
+
     override fun load(context: Context) {
         registerMainAPI(OppadramaProvider())
-        registerExtractorAPI(Smoothpre())
-        registerExtractorAPI(EmturbovidExtractor())
-        registerExtractorAPI(BuzzServer())
     }
+
 }
